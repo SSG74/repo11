331cd5fe2e453189b40c2d1dbb408e18eea23223
Svutlana <fire_fly_74@mail.ru>
2023-01-29 14:10:39 +0500

    testR11

---
+++ testR11/Program.cs
@@ -0,0 +1,38 @@
+﻿// Написать программу, которая из имеющегося массива строк
+// формирует массив из строк, длина которых меньше либо
+// равна трем символам.
+
+string[] TruncateMassive(string[] Massive)
+{
+    string[] arr = new string[Massive.Length];
+
+    int j=0;
+    for (int i = 0; i < Massive.Length; i++)
+    {
+         if (Massive[i].Length<4)
+         {
+            arr[j] = Massive[i];
+            j++;
+         }  
+    }
+    Array.Resize(ref arr , j);
+    return arr;
+}
+
+string[] CreateMassive(int count)
+{
+    string[] arr = new string[count];
+    arr[0]="day";
+    arr[1]="4251";
+    arr[2]="birthday";
+    arr[3]=":)";
+    arr[4]="no";
+    return arr;
+}
+
+// Задать массив заполненный строками
+string[] array = CreateMassive(5);
+// определить длину строки
+// создать второй массив
+string[] NewArray = TruncateMassive(array);
+Console.WriteLine("["+ String.Join(", ", array) + "]->[" + String.Join (", ", NewArray)+ "]");
